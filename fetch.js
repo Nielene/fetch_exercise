ed8b8594f1b25e31a14a13e311e2b827cd20c3f7
Nielene Thomas <nielenethomas@pursuit.org>
2018-11-19 16:31:27 -0500

    5th commit

--- fetch.js
+++ fetch.js
@@ -3,14 +3,15 @@ document.addEventListener("DOMContentLoaded", () => {
   let button = document.querySelector("button");
   let body = document.querySelector("body");
   let image = document.createElement("img");
-  let src = image.getAttribute("src")
+  // let src = image.getAttribute("src")
 
-  image.setAttribute("src", "https://images.dog.ceo/breeds/corgi-cardigan/n02113186_13256.jpg");
+
+  image.src = "https://images.dog.ceo/breeds/corgi-cardigan/n02113186_13256.jpg";
 
   let dogObj;
   let response;
-  let parsedRes;
-  console.log(src);
+  let res;
+
 
   body.appendChild(image);
 
@@ -19,19 +20,9 @@ document.addEventListener("DOMContentLoaded", () => {
   function getRandomImage () {
     fetch("https://dog.ceo/api/breeds/image/random").then(response => {
       return response.json();
-      debugger;
-    }).then(parsedRes => {
-      // if (parsedRes) {
-        // body.appendChild(image);
-      // } else
-      if (parsedRes) {
-        // body.appendChild(image);
-        // document.querySelector(image).replaceChild(image.src, image)
-        document.querySelector(image).replaceChild(image.src, parsedRes.message)
+    }).then(res => {
+      image.src = res.message
 
-      }
-      console.log(parsedRes.message);
-      // console.log(response);
     })
 
     // image.src = dogObj.message;
