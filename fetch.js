25c3a6879f559a4df245a9fd8d27924a3edd95fe
Nielene Thomas <nielenethomas@pursuit.org>
2018-11-19 16:35:11 -0500

    5.5th commit

--- fetch.js
+++ fetch.js
@@ -3,18 +3,10 @@ document.addEventListener("DOMContentLoaded", () => {
   let button = document.querySelector("button");
   let body = document.querySelector("body");
   let image = document.createElement("img");
-  // let src = image.getAttribute("src")
-
 
   image.src = "https://images.dog.ceo/breeds/corgi-cardigan/n02113186_13256.jpg";
 
-  let dogObj;
-  let response;
-  let res;
-
-
   body.appendChild(image);
-
 
 
   function getRandomImage () {
@@ -22,17 +14,10 @@ document.addEventListener("DOMContentLoaded", () => {
       return response.json();
     }).then(res => {
       image.src = res.message
-
     })
-
-    // image.src = dogObj.message;
-
   }
-
 
   button.addEventListener("click", getRandomImage)
       // getRandomImage()
-
-
 
 })
