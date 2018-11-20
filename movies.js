eafcae30e13c1189eeb37bfb65b1578e5d615051
Nielene Thomas <nielenethomas@pursuit.org>
2018-11-19 19:33:09 -0500

    7.5th commit

--- movies.js
+++ movies.js
@@ -5,14 +5,14 @@ document.addEventListener("DOMContentLoaded", () => {
   let form = document.querySelector("form");
   let select = document.createElement("select");
   let firstOption = document.createElement('option');
-  let div = document.createElement("div");
-
+  let div;
+  let display;
 
   firstOption.innerText = ''
   select.appendChild(firstOption)
   form.appendChild(select);
-  body.appendChild(div);
-  div.classList.add("display");
+
+
 
   // ul.appendChild(li);
 
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }).then(res => {
 
       for (let i = 0; i < res.length; i++) {
-        // console.log(res[i]);
+        console.log(res[i]);
         // console.log(res[i].title);
         let option = document.createElement("option")
         option.innerText = res[i].title;
@@ -45,14 +45,22 @@ document.addEventListener("DOMContentLoaded", () => {
       return response.json();
     }).then(res => {
 
-      let ul = document.createElement("ul");
-      let li = document.createElement("li")
+      let ul =document.createElement("ul");
+      let li =document.createElement("li");
+      div = document.createElement("div");
+      display = document.querySelector(".display");
 
       for (let i = 0; i < res.length; i++) {
-        li.innerText = res[i].title
+        li.innerText = res[i].title;
+        li.innerText = res[i].director;
+        li.innerText = res[i].description;
+        // li.innerText = res[i].
+        // li.innerText = res[i].
+        div.classList.add("display");
 
         ul.appendChild(li);
-        body.appendChild(ul);
+        display.appendChild(ul);
+        body.appendChild(div);
 
     }
     console.log(ul);
